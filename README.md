d2439873be6574bceb94e9a387e36ba30ec9722e
Esteban Angel Mesa <estebanangelm@dhcp-128-189-94-16.ubcsecure.wireless.ubc.ca>
2017-11-18 08:32:38 -0800

    Add project_generator files

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# project_generator
-Template for generating new data science projects.
+## project_generator
+
+A terminal script for creating an organized repository for a new data science project.
+
+### Instructions
+
+After cloning or downloading the `project_generator` repo, move the `project_generator.sh` file to the location in which you want to create your data science project. When you have the file in the location, execute the following line in the terminal:
+
+```
+bash project_generator.sh
+
+```
+Follow the instructions, you will only have to type the project name and select the type of license for your project.
+
+### Directory Structure
+
+After executing this script, your project directory will have the following structure:
+
+
+```
+├── LICENSE
+├── README.md          <- A README file with a suggested structure.
+├── data
+│   ├── sql_queries    <- A folder for including the SQL scripts if needed.
+│   ├── processed      <- The data after the wrangling process.
+│   └── original            <- The original datasets.
+│
+├── doc               	<- A folder for including documentation for executing the analysis.
+│
+│
+├── notebooks          <- Jupyter or Rmarkdown notebooks.
+│
+├── results            <- PDF, Markdown or HTML files with the results.
+│   └── figures        <- Folder with the figures and plots.
+│
+├── src                <- Scripts and code used for the project.
+	│
+	├── data           <- Scripts to download or wrangle the data
+	│
+	├── models         <- Scripts to train models.
+```
+
+### Author and Creation Date
+
+This script was created by @estebanangelm in 2017.
--- project_generator.sh
+++ project_generator.sh
@@ -1,69 +1,89 @@
 #!/bin/bash
-read -p "Enter Your Project Name: "  project_name
+
+while [[ -z "$project_name" ]]
+do
+  read -p "Enter Your Project Name: "  project_name
+done
 
 mkdir $project_name
 cd $project_name
 
+mkdir "doc"
+mkdir "notebooks"
 
 mkdir "data"
-mkdir "doc"
-mkdir "models"
-mkdir "notebooks"
+cd "data"
+mkdir "sql_queries"
+mkdir "processed"
+mkdir "original"
+cd ..
+
 mkdir "results"
+cd "results"
+mkdir "figures"
+cd ..
+
 mkdir "src"
+cd "src"
+mkdir "data"
+mkdir "models"
+cd ..
 
-echo "# README
+echo "# $project_name
+Put some information about your data science project! I recommend you a structure:
 
-    Put some information about your data science project! I recommend you a structure:
+## Problem
 
-    ## Problem
+In a couple of sentences write the problem you're trying to solve using data.
 
-    In a couple of sentences write the problem you're trying to solve using data.
+## Hypothesis
 
-    ## Hypothesis
+State your main hypothesis.
 
-    State your main hypothesis of the research.
+## Requirements
 
-    ## Data Sources
+Explain the technical requirements the user cloning your repository needs for reproducing the project.
 
-    There's a directory just for data sources, but it's important to have a summary table with the main ones:
-    Here's also a table for adding the important files and their routes inside the repository:
+## Data Sources
 
+There's a directory just for data sources, but it's important to have a summary table with the main ones:
 
-    | File Name | Address | Description |
-    |:-:|:-:|:-:|:-:|
-    |  |  |  |  |
-    |  |  |  |  |
-    |  |  |  |  |" > README.md
+| File Name | Address | Description |
+|:-:|:-:|:-:|:-:|
+|  |  |  |  |
+|  |  |  |  |
+|  |  |  |  |" > README.md
 
-PS3='Please select your license type: '
+PS3='Please select a license type for your project (just type the number): '
 options=("MIT" "BSD" "Empty")
 select opt in "${options[@]}"
 do
     case $opt in
         "MIT")
             echo "# LICENSE
-            Copyright (c) ((NAME))((YEAR))
+MIT License
 
-            Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+Copyright (c) ((NAME))((YEAR))
 
-            The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-            THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-          " > LICENSE.md
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+" > LICENSE.md
           break;;
         "BSD")
             echo "# LICENSE
-              Copyright <YEAR> <COPYRIGHT HOLDER>
+Copyright <YEAR> <COPYRIGHT HOLDER>
 
-              Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
-              1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 
-              2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 
-              THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-              " > LICENSE.md
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+" > LICENSE.md
           break;;
         "Empty")
             echo "# LICENSE" > LICENSE.md
